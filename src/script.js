05d8a80cdd9a02b5b59c5210b46d05b243192f5d
Maxine15x <101028209+Maxine15x@users.noreply.github.com>
2022-06-18 17:14:51 -0400

    Improved search engine

--- src/script.js
+++ src/script.js
@@ -1,48 +1,58 @@
-function currentDate(date) {
-  let hours = date.getHours();
-  if (hours < 12) {
-    hours = `${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 59) {
-    minutes = `${minutes}`;
-  }
+function currentDate(timestamp) {
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+	if (hours < 10) {
+		hours = `0${hours}`;
+	}
+	let minutes = date.getMinutes();
+	if (minutes < 10) {
+		minutes = `0${minutes}`;
+	}
 
-  let eachDay = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = days[eachDay];
-
-  return `${day} ${hours}:${minutes}`;
+	let days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+	];
+	let day = days[date.eachDay()];
+	return `${day} ${hours}:${minutes}`;
 }
 
-let h2 = document.querySelector("h2");
-let newTime = new Date();
-h2.innerHTML = currentDate(newTime);
+function currrentDay(timestamp) {
+	console.log(date);
+	let date = new Date(timestamp * 1000);
+	let day = date.eachDay();
+	let days = ["Sun", "Mon", "Tues", "Wed", "Thu", "Fri", "Sat"];
+
+	return days[day];
+}
 
 function showTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let city = response.data.name;
-  let h1 = document.querySelector("h1");
-  h1.innerHTML = `${city}`;
-  let h3 = document.querySelector("h3");
-  h3.innerHTML = `${temperature}`;
+	let temperature = Math.round(response.data.main.temp);
+	let city = response.data.name;
+	let h1 = document.querySelector("h1");
+	h1.innerHTML = `${city}`;
+	let h3 = document.querySelector("h3");
+	h3.innerHTML = `${temperature}`;
+}
+
+function searchCity(city) {
+	let apiKey = "769668820f2d4467990d718542c608e8";
+	let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+	axios.get(apiUrl).then(showTemperature);
 }
 
 function searchEngine(event) {
-  event.preventDefault();
-  let apiKey = "769668820f2d4467990d718542c608e8";
-  let city = document.querySelector("#search-location").value;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(showTemperature);
+	event.preventDefault();
+	let cityLocation = document.querySelector("#search-location");
+	search(cityLocation.value);
 }
 
 let searchInput = document.querySelector("#find-city");
 searchInput.addEventListener("submit", searchEngine);
+
+search("Clearwater");
